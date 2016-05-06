36fdf829b19717efd975cf358daba31d5ff60e04
xiao233ming <dianziwanwan@163.com>
2016-05-06 20:27:26 +0800

    update

--- app/src/main/java/com/example/mingming/lightsensortest/MainActivity.java
+++ app/src/main/java/com/example/mingming/lightsensortest/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.mingming.lightsensortest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //声明传感器监听器
+    private SensorManager sensorManager;
+
+    private TextView lightLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lightLevel = (TextView) findViewById(R.id.light_level);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        //注册监听器
+        sensorManager.registerListener(listener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager !=null){
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            //values数组中第一个下标的值就是当前的光照强度
+            float value = event.values[0];
+            lightLevel.setText("Current light level is" + value + "1x" );
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
